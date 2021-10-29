b25746dcb5cc0fb4f5f81da88b40557ff7f7198a
Alexander2804 <67113804+Alexander2804@users.noreply.github.com>
2021-10-29 11:37:44 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+botão de  importação de  './Button.js'
+importar  contêiner  de  './Container.js'
+importar  texto  de  './Text.js'
+importar  janela  de  './Window.js'
+
+export  default  {
+    Botão ,
+    Container ,
+    Texto ,
+    Janela
+}
